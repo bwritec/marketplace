3fdde36475f81fe14ecfb7804a1bd9c9cf4fb6c5
Jorge Luiz <bjxjorge@gmail.com>
2026-04-18 00:19:47 -0300

    .

--- app/Views/system/dashboard/favorites.php
+++ app/Views/system/dashboard/favorites.php
@@ -29,11 +29,40 @@
     </section>
 
     <section class="content">
-        <div class="card">
-            <div class="card-body">
-                Start creating your amazing application!
+        <?php if (count($favorites) == 0): ?>
+            <div class="card">
+                <div class="card-body">
+                    Nenhum favorito disponível
+                </div>
             </div>
-        </div>
+        <?php else: ?>
+            <?php foreach ($favorites as $item): ?>
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <!-- imagem -->
+                        <?php if (!empty($item['thumbnail'])): ?>
+                            <div style="float: left;">
+                                <img src="<?= base_url() ?><?= $item['thumbnail'] ?>" class="card-img-top" alt="<?= esc($item['name']) ?>" style="width: 150px; margin-right: 1rem; display: block;">
+                            </div>
+
+                            <h5 class="card-title">
+                                <?= esc($item['name']) ?>
+                            </h5>
+
+                            <?php if (!empty($item['price'])): ?>
+                                <p class="card-text">
+                                    <strong>R$ <?= number_format($item['price'], 2, ',', '.') ?></strong>
+                                </p>
+                            <?php endif; ?>
+
+                            <a href="<?= site_url("product") ?>/<?= $item['id'] ?>" class="btn btn-primary">
+                                Ver Produto
+                            </a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </section>
 </div>
 <?= $this->endSection() ?>
