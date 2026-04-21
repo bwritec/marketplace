5cdae835aad6aed2c5cf58defb17efd3505dbecf
Jorge Luiz <bjxjorge@gmail.com>
2026-04-21 06:30:18 -0300

    .

--- app/Views/system/layouts/dashboard.php
+++ app/Views/system/layouts/dashboard.php
@@ -108,7 +108,7 @@
             <div class="sidebar">
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="<?= base_url() ?>system/dist/img/user-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="<?= base_url() ?>dist/img/user-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                     </div>
 
                     <div class="info">
